95ac1ca5cecff021b77421c3f323d531d7602ce8
Vincent Putra <vincentputrago@gmail.com>
2025-05-19 00:22:11 +0700

    Update employee page

--- composables/useUsers.ts
+++ composables/useUsers.ts
@@ -6,7 +6,9 @@ type User = {
   id: number;
   name: string;
   email: string;
+  phone: string;
   role: number;
+  password: string;
 };
 
 type Role = {
@@ -14,8 +16,24 @@ type Role = {
   name: string;
 };
 
+type Page = {
+  page: number;
+  per_page: number;
+};
+
 export const useUsers = () => {
   const users = ref<User[]>([]);
+  const pageUser = ref<{
+    current_page: number;
+    last_page: number;
+    per_page: number;
+    total: number;
+  }>({
+    current_page: 1,
+    last_page: 1,
+    per_page: 10,
+    total: 0,
+  });
   const isLoading = ref(false);
   const error = ref<string | null>(null);
   const storage = useStorage();
@@ -31,7 +49,7 @@ export const useUsers = () => {
     },
   ]);
 
-  const fetchUsers = async () => {
+  const fetchUsers = async (payload: Page) => {
     isLoading.value = true;
     error.value = null;
 
@@ -48,17 +66,26 @@ export const useUsers = () => {
 
       const { data, error } = await useFetch<{
         data: User[];
-      }>(`${config.public.apiBase}/users`, {
-        headers: {
-          Authorization: `Bearer ${tokenData.value}`,
-          Accept: "application/json",
-        },
-        lazy: true,
-        server: false,
-      });
+        meta: {
+          current_page: number;
+          last_page: number;
+          per_page: number;
+          total: number;
+        };
+      }>(
+        `${config.public.apiBase}/users?page=${payload.page}&per_page=${payload.per_page}`,
+        {
+          headers: {
+            Authorization: `Bearer ${tokenData.value}`,
+            Accept: "application/json",
+          },
+          lazy: true,
+          server: false,
+        }
+      );
 
       if (error.value) {
-        throw new Error(error.value.message || "Failed to fetch users");
+        throw new Error(error.value.data.message || "Failed to fetch users");
       }
 
       if (!data.value?.data) {
@@ -66,9 +93,140 @@ export const useUsers = () => {
       }
 
       users.value = data.value.data;
+      pageUser.value = data.value.meta;
     } catch (e) {
       error.value = e instanceof Error ? e.message : "Failed to fetch users";
-      console.error("Error fetching users:", e);
+      throw new Error(error.value);
+      // error.value == "Unauthenticated."
+      //console.error("Error fetching users:", e);
+    } finally {
+      isLoading.value = false;
+    }
+  };
+
+  const createUser = async (payload: any) => {
+    isLoading.value = true;
+    error.value = null;
+
+    try {
+      const storedToken = storage.getItem("auth_token");
+      if (!storedToken) throw new Error("No auth token found");
+
+      let tokenData: { value: string; expiresAt: number };
+      try {
+        tokenData = JSON.parse(storedToken);
+      } catch {
+        throw new Error("Invalid token format");
+      }
+
+      const { data, error } = await useFetch(`${config.public.apiBase}/users`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${tokenData.value}`,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: payload,
+      });
+
+      if (error.value) {
+        throw new Error(error.value.data.message || "Failed to create user");
+      }
+
+      if (!data.value) {
+        throw new Error("No response data received");
+      }
+
+      return data.value;
+    } catch (e) {
+      error.value = e instanceof Error ? e.message : "Failed to create user";
+      throw new Error(error.value);
+      //console.error("Error creating user:", e);
+    } finally {
+      isLoading.value = false;
+    }
+  };
+
+  const updateUser = async (payload: any) => {
+    isLoading.value = true;
+    error.value = null;
+
+    try {
+      const storedToken = storage.getItem("auth_token");
+      if (!storedToken) throw new Error("No auth token found");
+
+      let tokenData: { value: string; expiresAt: number };
+      try {
+        tokenData = JSON.parse(storedToken);
+      } catch {
+        throw new Error("Invalid token format");
+      }
+
+      const { data, error } = await useFetch(
+        `${config.public.apiBase}/users/${payload.id}`,
+        {
+          method: "PATCH",
+          headers: {
+            Authorization: `Bearer ${tokenData.value}`,
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: payload,
+        }
+      );
+
+      if (error.value) {
+        throw new Error(error.value.data.message || "Failed to update user");
+      }
+
+      if (!data.value) {
+        throw new Error("No response data received");
+      }
+
+      return data.value;
+    } catch (e) {
+      error.value = e instanceof Error ? e.message : "Failed to update user";
+      throw new Error(error.value);
+      //console.error("Error updating user:", e);
+    } finally {
+      isLoading.value = false;
+    }
+  };
+
+  const deleteUser = async (payload: any) => {
+    isLoading.value = true;
+    error.value = null;
+    try {
+      const storedToken = storage.getItem("auth_token");
+      if (!storedToken) throw new Error("No auth token found");
+      let tokenData: { value: string; expiresAt: number };
+      try {
+        tokenData = JSON.parse(storedToken);
+      } catch {
+        throw new Error("Invalid token format");
+      }
+      const { data, error } = await useFetch(
+        `${config.public.apiBase}/users/${payload.id}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${tokenData.value}`,
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (error.value) {
+        throw new Error(error.value.data.message || "Failed to delete user");
+      }
+      if (!data.value) {
+        throw new Error("No response data received");
+      }
+      return data.value;
+    } catch (e) {
+      error.value = e instanceof Error ? e.message : "Failed to delete user";
+      throw new Error(error.value);
+      //console.error("Error deleting user:", e);
     } finally {
       isLoading.value = false;
     }
@@ -76,9 +234,13 @@ export const useUsers = () => {
 
   return {
     users,
+    pageUser,
     roles,
     isLoading,
     error,
     fetchUsers,
+    createUser,
+    updateUser,
+    deleteUser,
   };
 };
--- pages/employees.vue
+++ pages/employees.vue
@@ -37,31 +37,59 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination";
 import { toast } from "vue-sonner";
-import { RefreshCcw, Plus } from "lucide-vue-next";
+import { RefreshCcw, Plus, User } from "lucide-vue-next";
 import { useUsers } from "@/composables/useUsers";
 
 interface User {
   id: number;
   name: string;
   email: string;
+  phone: string;
   role: number;
 }
 
-const { users, roles, isLoading, fetchUsers } = useUsers();
+const {
+  users,
+  pageUser,
+  roles,
+  isLoading,
+  error,
+  fetchUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+} = useUsers();
+
+const currentPage = ref(1);
+const itemsPerPage = ref(15);
+
+const handlePageChange = async (page: number) => {
+  currentPage.value = page;
+  await fetchUsers({
+    page: currentPage.value,
+    per_page: itemsPerPage.value,
+  });
+};
 
 onMounted(() => {
   nextTick(async () => {
-    await fetchUsers();
+    await fetchUsers({
+      page: currentPage.value,
+      per_page: itemsPerPage.value,
+    });
   });
 });
 
 const isModalOpen = ref(false);
 const isEditing = ref(false);
+const isDelete = ref(false);
 const editingId = ref<number | null>(null);
 
 const form = reactive({
   name: "",
   email: "",
+  phone: "",
+  password: "",
   role: 0,
 });
 
@@ -72,11 +100,18 @@ const openAddModal = () => {
   isModalOpen.value = true;
 };
 
-const editUser = (user: User) => {
+const openDeleteModal = (id: number) => {
+  isDelete.value = true;
+  editingId.value = id;
+  isModalOpen.value = true;
+};
+
+const openEditModal = (user: User) => {
   isEditing.value = true;
   editingId.value = user.id;
   form.name = user.name;
   form.email = user.email;
+  form.phone = user.phone;
   form.role = user.role;
   isModalOpen.value = true;
 };
@@ -89,42 +124,87 @@ const closeModal = () => {
 const resetForm = () => {
   form.name = "";
   form.email = "";
+  form.phone = "";
+  form.password = "";
   form.role = 0;
 };
 
-const handleSubmit = () => {
-  // Here you would typically make an API call to save the category
+const handleSubmit = async () => {
   if (isEditing.value) {
-    // Update existing category
+    // Here you would typically make an API call to update the user
+    await updateUser({
+      id: editingId.value,
+      name: form.name,
+      email: form.email,
+      phone: form.phone,
+      role: form.role,
+      password: form.password,
+    });
+
+    if (error.value) {
+      toast.error(error.value ?? "Something went wrong");
+      return;
+    }
+
+    // Update existing user
     const index = users.value.findIndex(
       (s) => Number(s.id) === editingId.value
     );
+
     if (index !== -1) {
       users.value[index] = {
         id: editingId.value!,
         name: form.name,
         email: form.email,
+        phone: form.phone,
         role: form.role,
+        password: form.password,
       };
     }
+
     toast.success("User updated successfully");
   } else {
-    // Add new user
-    users.value.push({
-      id: Date.now(),
+    // Here you would typically make an API call to add the user
+    const response = await createUser({
       name: form.name,
       email: form.email,
+      phone: form.phone,
       role: form.role,
+      password: form.password,
     });
+
+    if (error.value) {
+      toast.error(error.value ?? "Something went wrong");
+      return;
+    }
+
+    // Add new user
+    users.value.push({
+      id: (response as any).id ?? Date.now(),
+      name: (response as any).name ?? form.name,
+      email: (response as any).email ?? form.email,
+      phone: (response as any).phone ?? form.phone,
+      role: (response as any).role,
+      password: form.password,
+    });
+
     toast.success("User added successfully");
   }
   closeModal();
 };
 
-const deleteUser = (id: number) => {
+const handleDelete = async () => {
+  await deleteUser({ id: editingId.value });
+
+  if (error.value) {
+    toast.error(error.value ?? "Something went wrong");
+    return;
+  }
+
   // Here you would typically make an API call to delete the user
-  users.value = users.value.filter((s) => s.id !== id);
+  users.value = users.value.filter((s) => s.id !== editingId.value);
   toast.success("User deleted successfully");
+  closeModal();
 };
 
 definePageMeta({
@@ -139,7 +219,16 @@ definePageMeta({
       <div class="flex items-center justify-between">
         <h1 class="text-2xl font-semibold text-gray-800">Employees</h1>
         <div class="flex items-center gap-2">
-          <Button variant="outline" size="sm" @click="fetchUsers">
+          <Button
+            variant="outline"
+            size="sm"
+            @click="
+              fetchUsers({
+                page: currentPage,
+                per_page: itemsPerPage,
+              })
+            "
+          >
             <RefreshCcw class="mr-2 h-4 w-4" />
             Refresh
           </Button>
@@ -158,14 +247,15 @@ definePageMeta({
             <TableRow>
               <TableHead>Name</TableHead>
               <TableHead>Email</TableHead>
+              <TableHead>Phone</TableHead>
               <TableHead>Role</TableHead>
               <TableHead>Actions</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             <template v-if="isLoading">
-              <TableRow v-for="n in 5" :key="n">
-                <TableCell v-for="m in 2" :key="m">
+              <TableRow v-for="n in pageUser.total ?? 5" :key="n">
+                <TableCell v-for="m in 5" :key="m">
                   <div class="h-4 w-24 animate-pulse rounded bg-gray-100" />
                 </TableCell>
               </TableRow>
@@ -174,18 +264,23 @@ definePageMeta({
               <TableRow v-for="user in users" :key="user.id">
                 <TableCell>{{ user.name }}</TableCell>
                 <TableCell>{{ user.email }}</TableCell>
+                <TableCell>{{ user.phone }}</TableCell>
                 <TableCell>{{
                   roles.find((r) => r.id === user.role)?.name
                 }}</TableCell>
                 <TableCell>
                   <div class="flex gap-2" v-if="user.id !== 0">
-                    <Button variant="outline" size="sm" @click="editUser(user)">
+                    <Button
+                      variant="outline"
+                      size="sm"
+                      @click="openEditModal(user)"
+                    >
                       Edit
                     </Button>
                     <Button
                       variant="destructive"
                       size="sm"
-                      @click="deleteUser(user.id)"
+                      @click="openDeleteModal(user.id)"
                     >
                       Delete
                     </Button>
@@ -199,12 +294,13 @@ definePageMeta({
 
       <Pagination
         v-slot="{ page }"
-        :items-per-page="10"
-        :total="100"
+        :items-per-page="itemsPerPage"
+        :total="pageUser.total"
+        :default-page="currentPage"
         :sibling-count="1"
+        :class="'mt-4'"
         show-edges
-        :default-page="2"
-        class="mt-5"
+        @update:page="handlePageChange"
       >
         <PaginationContent v-slot="{ items }" class="flex items-center gap-1">
           <PaginationFirst />
@@ -214,14 +310,9 @@ definePageMeta({
               v-if="item.type === 'page'"
               :key="index"
               :value="item.value"
-              as-child
+              :is-active="item.value === page"
             >
-              <Button
-                class="w-10 h-10 p-0"
-                :variant="item.value === page ? 'default' : 'outline'"
-              >
-                {{ item.value }}
-              </Button>
+              {{ item.value }}
             </PaginationItem>
             <PaginationEllipsis v-else :key="item.type" :index="index" />
           </template>
@@ -233,7 +324,7 @@ definePageMeta({
 
     <!-- Add/Edit Modal -->
     <Dialog :open="isModalOpen" @update:open="closeModal">
-      <DialogContent>
+      <DialogContent v-if="!isDelete">
         <DialogHeader>
           <DialogTitle>{{
             isEditing ? "Edit Employee" : "Add Employee"
@@ -247,6 +338,29 @@ definePageMeta({
           <div class="space-y-2">
             <Label for="email">Email</Label>
             <Input id="email" type="email" v-model="form.email" required />
+          </div>
+          <div class="space-y-2">
+            <Label for="phone">Phone</Label>
+            <Input
+              id="phone"
+              type="tel"
+              pattern="[0-9]+"
+              v-model="form.phone"
+              required
+            />
+          </div>
+          <div class="space-y-2">
+            <Label for="password" v-if="!isEditing">Password</Label>
+            <Label for="password" v-else
+              >Password (Leave it empty if you don't need to update the
+              password)</Label
+            >
+            <Input
+              id="password"
+              type="password"
+              v-model="form.password"
+              v-bind:required="!isEditing"
+            />
           </div>
           <div class="space-y-2">
             <Label for="name">Role</Label>
@@ -267,6 +381,9 @@ definePageMeta({
               </SelectContent>
             </Select>
           </div>
+          <div v-if="error" class="mt-2 text-sm text-red-600">
+            {{ error }}
+          </div>
           <DialogFooter>
             <Button type="button" variant="outline" @click="closeModal"
               >Cancel</Button
@@ -274,6 +391,21 @@ definePageMeta({
             <Button type="submit">{{ isEditing ? "Update" : "Add" }}</Button>
           </DialogFooter>
         </form>
+      </DialogContent>
+      <DialogContent v-else>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. Are you sure you want to permanently
+            delete this file from our servers?
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button type="button" variant="outline" @click="closeModal"
+            >Cancel</Button
+          >
+          <Button type="submit" @click="handleDelete">Confirm</Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   </div>
